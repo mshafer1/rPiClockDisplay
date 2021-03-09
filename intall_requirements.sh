3d54fb2650ac0149826a124cd25e17d62f2cbcc4
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-03-08 20:20:30 -0600

    update requirements

--- intall_requirements.sh
+++ intall_requirements.sh
@@ -1,1 +1,11 @@
-sudo apt-get install -y i2c-tools python-smbus pigpio python-pigpio python3-pigpio
+sudo apt-get install -y \
+ python3-pip \
+ python3-pil \
+ libjpeg-dev \
+ zlib1g-dev \
+ libfreetype6-dev \
+ liblcms2-dev \
+ libopenjp2-7 \
+ libtiff5 \
+
+sudo usermod -a -G spi,gpio,i2c pi
